2a8e6c20a67b82140eb75e01b22be124bf6d74e2
Baris DINC <barisdinc@gmail.com>
2020-01-19 00:03:35 +0300

    GPS okuma ve Seri port rutinleri degistirildi
    gps okumak icin nmae NeoGPS kutuphanesine gecildi
    seri port icin neoSWserial kutuphanesi ve interrupt yapisina gecildi

--- hymTR.ino
+++ hymTR.ino
@@ -1,186 +1,154 @@
 /*
  * hymTR 2020
  * Baris DINC - TA7W
+ * 10 Ocak 2020
  * APRS Tracker
  */
-#include <SoftwareSerial.h>
-#include <SimpleTimer.h>
-//#include <TinyGPS.h>
-#include <Adafruit_GPS.h>
-#include <LibAPRS.h>
+//Pin tanimlamalari kutuphane include larindan once yapilmalidir.
+#define RX_PIN 11  //GPS TX pin 
+#define TX_PIN 12  //GPS RX pin
+
+#include <LibAPRS_Tracker.h>
+#include <NMEAGPS.h>
+#include <GPSport.h>
 #include <EEPROM.h>
 
+/*
+ * Notlar:
+ * NeoGPS kutuphanesinde su duzenlemeleri yapiniz;
+ *  GPSport.h icinde #include <NeoSWSerial.h> satirini acip #include <AltSoftSerial.h> satirini kapatiniz
+ *  NMEAGPS_cfg.h icinde NMEAGPS_INTERRUPT_PROCESSING define satirini acmaniz ya da burda tanimlamaniz gerekir
+ */
+
 //Genel Tanimlamalar
-#define VERSIYON "ABCD"
+#define VERSIYON "01012020a"
 
-// GPS SoftwareSerial
-// GPS'in bagli oldugu PIN'ler
-#define GPS_RX_PIN 12
-#define GPS_TX_PIN 11
+//Kutuphane kontrolleri
+#if !defined(GPS_FIX_TIME) | !defined(GPS_FIX_DATE)
+  #error NeoGPS kutuphanesinde GPSfix_cfg.h icinde  GPS_FIX_TIME ve DATE define edilmelidir
+#endif
+#if !defined(NMEAGPS_PARSE_RMC) & !defined(NMEAGPS_PARSE_ZDA)
+  #error You must define NMEAGPS_PARSE_RMC or ZDA in NMEAGPS_cfg.h!
+#endif
 
-//Test butonu manuel gonderme islemi icindir
-#define TEST_BUTTON_PIN 10
-
-// PTT_PIN'i libAPRS/device.h icinde tanimlanmistir
-#define PTT_PIN 3
-
-// LibAPRS
-#define OPEN_SQUELCH false
-#define ADC_REFERENCE REF_3V3  // Bu deger Nano icin 3V3 , uno icin 5V olacak
-
-//char mesaj []= "/A=001234 TAMSAT hymTR APRS Tracker";
+NMEAGPS  gps; // This parses the GPS characters
+gps_fix  fix; // This holds on to the latest values
+char comment []= "TAMSAT hymTR APRS Tracker Test";
 
 struct APRS_Ayarlari {
   char APRS_CagriIsareti[7];
   char APRS_CagriIsaretiSSID;
-//  char APRS_Destination[7];
-//  char APRS_DestinationSSID;  
-//  char APRS_Path1[7];
-//  char APRS_Path1SSID;
-//  char APRS_Path2[7];
-//  char APRS_Path2SSID;
+  char APRS_Destination[7];
+  char APRS_DestinationSSID;  
+  char APRS_Path1[7];
+  char APRS_Path1SSID;
+  char APRS_Path2[7];
+  char APRS_Path2SSID;
   char APRS_Sembolu;
-//  char APRS_SembolTabi;
-//  byte APRS_BeaconTipi;    //0=sure beklemeli, 1=Smart Beacon
+  char APRS_SembolTabi;
+  byte APRS_BeaconTipi;    //0=sure beklemeli, 1=Smart Beacon
   unsigned long APRS_BeaconSuresi;
-//  unsigned int  APRS_GPSSeriHizi;    
-  char APRS_Mesaj[11]; //TODO 41 olacak
+  unsigned int  APRS_GPSSeriHizi;    
+  char APRS_Mesaj[41];
   unsigned int CheckSum;    //Cagri isaretinin byte toplamini kullaniyoruz
 };
-
 APRS_Ayarlari Ayarlar;
 
-// Gonderme zamanlayicisi olarak kullanilan Timer
-#define TIMER_DISABLED -1
-#define TIMER_MINUTES 10000 //60L*1000L
+//forward declarations - fonksiyon tanimlarimiz
+void locationUpdate();
+char* deg_to_nmea(long deg, boolean is_lat);
+void eepromOku();
+void eepromaYaz();
+void VarsayilanAyarlar();
+void konfigurasyonYazdir();
+void parametreOku(char *szParam, int iMaxLen);
+bool seridenAl();
+void seriyeGonder();
 
-SoftwareSerial GPSSerial(GPS_RX_PIN, GPS_TX_PIN);
-//TinyGPS gps;
-Adafruit_GPS GPS(&GPSSerial);
-
-SimpleTimer timer;
-
-char aprs_update_timer_id = TIMER_DISABLED;
-bool send_aprs_update = false;
-
-//long lat = 0;
-//long lon = 0;
-//long alt = 0;
-//long gps_course = 0;
-//long gps_speed = 0;
-
-//int year=0;
-//byte month=0, day=0, hour=0, minute=0, second=0, hundredths=0;
-//unsigned long age=0;
-
-bool newData = false;
-bool ilkAcilis = true;
-
-//Fonksiyonlarimiz
-//void eepromOku();
-
-void setup()  
+static void GPSisr( uint8_t c )
 {
-   eepromOku();
-  
-  Serial.begin(115200);   //Bilgisayar haberlesme portu
-  GPSSerial.begin(9600);  //GPS veri okuma portu
-  //TODO: ayarlanan parametreyi kullan
+  gps.handle( c );
+} // GPSisr
 
-  pinMode(TEST_BUTTON_PIN, INPUT_PULLUP);
-  pinMode( 13, OUTPUT ); //PIN13????
-  
-  Serial.println(F("TAMSAT hymTR APRS Tracker"));
+void setup()
+{
+  eepromOku();
+  APRS_init();
+  char myCALL[] = "TA7W";
+  char Lat[] = "2134.00N";
+  char Lon[] = "01234.00E";
+  APRS_setCallsign(myCALL, 9);
+  APRS_setLat(Lat);
+  APRS_setLon(Lon);
+  //APRS_setPreamble(1000);
+  //APRS_setTail(50);
 
-  APRS_init(ADC_REFERENCE, OPEN_SQUELCH);
-  APRS_setPreamble(350); //TODO: Konfigurasyondan kullan
-  APRS_setTail(50);      //TODO: Konfigurasyondan kullan
-  APRS_setCallsign(Ayarlar.APRS_CagriIsareti,Ayarlar.APRS_CagriIsaretiSSID);
-  APRS_setSymbol(Ayarlar.APRS_Sembolu);
-  
-//  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-//  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-//  GPS.sendCommand(PGCMD_ANTENNA);  
-  
-  aprs_update_timer_id=timer.setInterval(2*TIMER_MINUTES, setAprsUpdateFlag);
+  DEBUG_PORT.begin(115200);
+  while (!Serial);
+  DEBUG_PORT.print( F("hymTR APRS Tracker\n") );
+  gpsPort.attachInterrupt( GPSisr );
+  gpsPort.begin(9600);
 }
+
+int getGpsData(boolean *newDataLoc)
+{
+ while (gps.available( gpsPort )) {
+    fix = gps.read();
+    if (fix.valid.location && fix.valid.time && fix.valid.altitude) {
+      *newDataLoc = true;
+      return fix.dateTime.minutes*60+fix.dateTime.seconds;
+    }
+  }
+return -1; 
+}
+
+
 
 void loop()
 {
-    /*
-     * Burada GPS datalarini okuyup anlamli bir veri olup olmadigina bakiyoruz
-     */
-    while (GPSSerial.available())
-    {
-      char c = GPS.read(); //GPSSerial.read();
-      //Serial.write(c); 
-      //if (gps.encode(c)) newData = true;
-      if (GPS.newNMEAreceived()) GPS.parse(GPS.lastNMEA());
-    }
+  boolean newData = false;
+  int gpsMinSec = getGpsData(&newData);
+  //if (gps.available()) {
+    // Print all the things!
+    //trace_all( DEBUG_PORT, gps, gps.read() );
+  //}
 
-    
-/*
-   if (newData)// & !send_aprs_update)
-    {
-    //gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, NULL, &age);
-    gps.get_position(&lat, &lon, &age);
-    alt = gps.altitude() / 3.281;
-    gps_course = gps.course();
-    gps_speed = gps.speed();
-    newData = false; //data islendi ve aktarildi
-    }  
-*/
-
-  if (send_aprs_update || digitalRead(TEST_BUTTON_PIN)==0) {
-    while(digitalRead(TEST_BUTTON_PIN)==0) {};
-    lokasyonGuncelle();
-    newData = false;
-//    Serial.println("new data");
-//    Serial.println(lat);
-//    Serial.println(alt);
+    if (gps.overrun()) {
+    gps.overrun( false );
+    DEBUG_PORT.println( F("DATA OVERRUN: took too long to print GPS data!") );
   }
-
-  /*
-   * APRS Timer saati...
-   */
-  timer.run();
-
-  /*
-   * Burada seri porttan komutlari okuyoruz
-   * TODO: bunu interrupt a tasimaliyiz
-   */
-  while (Serial.available())
-  {
-    char komut = Serial.read();
-     if (komut == '!') {
-      KomutSatiriCalistir();
-      }
-  }
-
-
-}
-
-void aprs_msg_callback(struct AX25Msg *msg) {
-}
-
-void lokasyonGuncelle() {
   
+  if (newData && ((gpsMinSec % 20) == 0))
+  {
+    newData = false;
+    DEBUG_PORT.println("sending....");
+    APRS_sendLoc(comment, strlen(comment));
+    while(bitRead(PORTB,5)); //Wait for transmission to be completed
+    DEBUG_PORT.println("sent....");
+    //delay(25000);
+  }
+  //DEBUG_PORT.print(".");
+
+}
+
+
+void locationUpdate() {
   //String alt = alt + 1000000;
   
-  APRS_setLat((char*)deg_to_nmea(GPS.latitude, true));
-  APRS_setLon((char*)deg_to_nmea(GPS.longitude, false));
-      
+  APRS_setLat((char*)deg_to_nmea(lat, true));
+  APRS_setLon((char*)deg_to_nmea(lon, false));
+
   GPSSerial.end();
   // TX
+  digitalWrite(LED_BUILTIN, HIGH);
   APRS_sendLoc(Ayarlar.APRS_Mesaj, strlen(Ayarlar.APRS_Mesaj));
+  digitalWrite(LED_BUILTIN, LOW);
   while(digitalRead(PTT_PIN));
-  GPSSerial.begin(9600); //TODO: ayarlanani kullan
+  GPSSerial.begin(9600);
   send_aprs_update = false;
 }
 
 char* deg_to_nmea(long deg, boolean is_lat) {
-  #define CONV_BUF_SIZE 16
-  static char conv_buf[CONV_BUF_SIZE];
   bool is_negative=0;
   if (deg < 0) is_negative=1;
 
@@ -203,7 +171,7 @@ char* deg_to_nmea(long deg, boolean is_lat) {
   snprintf(conv_buf + 6, 3, "%02lu", b);
   conv_buf[9] = '\0';
   if (is_lat) {
-    if (is_negative) {conv_buf[8]='S';}
+    if (is_negative) { [8]='S';}
     else conv_buf[8]='N';
     return conv_buf+1;
     // conv_buf +1 because we want to omit the leading zero
@@ -213,10 +181,6 @@ char* deg_to_nmea(long deg, boolean is_lat) {
     else conv_buf[8]='E';
     return conv_buf;
     }
-}
-
-void setAprsUpdateFlag() {
-  send_aprs_update = true;
 }
 
 /*
@@ -236,27 +200,6 @@ void eepromOku() {
   Serial.println(F("EEPROM'dan okuma tamamlandi"));
 }
 
-
-void VarsayilanAyarlar() {
-  strcpy(Ayarlar.APRS_CagriIsareti, "TAMSAT");
-  Ayarlar.APRS_CagriIsaretiSSID = '9';
-/*  strcpy(Ayarlar.APRS_Destination, "APRS  ");
-  Ayarlar.APRS_DestinationSSID = '0';
-  strcpy(Ayarlar.APRS_Path1, "WIDE1 ");
-  Ayarlar.APRS_Path1SSID = '1';
-  strcpy(Ayarlar.APRS_Path2, "WIDE2 ");
-  Ayarlar.APRS_Path2SSID = '1';
-*/  
-  Ayarlar.APRS_Sembolu = '>';
-//  Ayarlar.APRS_SembolTabi = '/';
-//  Ayarlar.APRS_BeaconTipi = 3;
-  Ayarlar.APRS_BeaconSuresi = 255;
-  strcpy(Ayarlar.APRS_Mesaj, "hymTR");
-//  Ayarlar.APRS_GPSSeriHizi = 9600;
-  Ayarlar.CheckSum = 458;  //TAMSAT icin Checksum degeri
-  eepromaYaz();
-}
-
 /*
  * CONFIG tipinde bir Struct i EEPROM a yaziyoruz
  */
@@ -264,7 +207,7 @@ void eepromaYaz() {
   for (unsigned int i=0; i<sizeof(Ayarlar); i++) {
     EEPROM.write(i, *((char*)&Ayarlar + i));
   }
-  //Serial.println("Eeproma yazildi");
+  Serial.println("Eeproma yazildi");
 }
 
 /*
@@ -276,50 +219,68 @@ void konfigurasyonYazdir()
   Serial.println("Mevcut Konfigurasyon Bilgileri");
   Serial.println("------------------------------");
   */
-/*  
-  //Serial.print("Cagri Isareti : ");  
+  //Serial.print(F("Cagri Isareti : "));  
   Serial.println(Ayarlar.APRS_CagriIsareti);
-  //Serial.print("         SSID : -"); 
+  //Serial.print(F("         SSID : -")); 
   Serial.println(Ayarlar.APRS_CagriIsaretiSSID);
-  //Serial.print("        Hedef : ");  
+  //Serial.print(F("        Hedef : "));  
   Serial.println(Ayarlar.APRS_Destination);
-  //Serial.print("   Hedef SSID : -"); 
+  //Serial.print(F("   Hedef SSID : -")); 
   Serial.println(Ayarlar.APRS_DestinationSSID);  
-  //Serial.print("        Path1 : ");  
+  //Serial.print(F("        Path1 : "));  
   Serial.println(Ayarlar.APRS_Path1);
-  //Serial.print("   Path1 SSID : -"); 
+  //Serial.print(F("   Path1 SSID : -")); 
   Serial.println(Ayarlar.APRS_Path1SSID);
-  //Serial.print("        Path2 : ");  
+  //Serial.print(F("        Path2 : "));  
   Serial.println(Ayarlar.APRS_Path2);
-  //Serial.print("   Path2 SSID : -"); 
+  //Serial.print(F("   Path2 SSID : -")); 
   Serial.println(Ayarlar.APRS_Path2SSID);
-  //Serial.print("       Sembol : ");  
+  //Serial.print(F("       Sembol : "));  
   Serial.println(Ayarlar.APRS_Sembolu);
-  //Serial.print("  Sembol Tipi : ");  
+  //Serial.print(F("  Sembol Tipi : "));  
   Serial.println(Ayarlar.APRS_SembolTabi);
-  //Serial.print("  Beacon Tipi : ");  
+  //Serial.print(F("  Beacon Tipi : "));  
   Serial.println(Ayarlar.APRS_BeaconTipi); //0=sure beklemeli, 1=Smart Beacon
-  //Serial.print("Beacon Suresi : ");  
+  //Serial.print(F("Beacon Suresi : "));  
   Serial.println(Ayarlar.APRS_BeaconSuresi);
-  //Serial.print("     GPS Hizi : ");  
+  //Serial.print(F("     GPS Hizi : "));  
   Serial.println(Ayarlar.APRS_GPSSeriHizi);    
-  //Serial.print("        Mesaj : ");  
+  //Serial.print(F("        Mesaj : "));  
   Serial.println(Ayarlar.APRS_Mesaj);
-  //Serial.print("Kontrol (CRC) : ");  
+  //Serial.print(F("Kontrol (CRC) : "));  
   Serial.println(Ayarlar.CheckSum);   
-*/  
+  
+}
+
+void VarsayilanAyarlar() {
+  strcpy(Ayarlar.APRS_CagriIsareti, "TA7W"); 
+  Ayarlar.APRS_CagriIsaretiSSID = '9';
+  strcpy(Ayarlar.APRS_Destination, "APRS  ");
+  Ayarlar.APRS_DestinationSSID = '0';
+  strcpy(Ayarlar.APRS_Path1, "WIDE1 ");
+  Ayarlar.APRS_Path1SSID = '1';
+  strcpy(Ayarlar.APRS_Path2, "WIDE2 ");
+  Ayarlar.APRS_Path2SSID = '1';
+  Ayarlar.APRS_Sembolu = '>';
+  Ayarlar.APRS_SembolTabi = '/';
+  Ayarlar.APRS_BeaconTipi = 3;
+  Ayarlar.APRS_BeaconSuresi = 255;
+  strcpy(Ayarlar.APRS_Mesaj, "TAMSAT hymTR APRS Tracker");
+  Ayarlar.APRS_GPSSeriHizi = 9600;
+  Ayarlar.CheckSum = 291;  //TAMSAT icin Checksum degeri
+  eepromaYaz();
 }
 
 void KomutSatiriCalistir() {
   byte komut;
 
   Serial.println(F("hymTR Konfigurasyon Arayuzu"));
-  Serial.print(F("#"));
-  //delay(50);
+  Serial.print('#');
+  delay(50);
 
   while (komut != 'Q') {
     //digitalWrite(13, !digitalRead(13)); //Gonderme yaptigimizda tracker i konsol modundan cikariyoruz
-    //delay(50);
+    delay(50);
     if (Serial.available()) {
       komut = Serial.read();
 
@@ -333,8 +294,10 @@ void KomutSatiriCalistir() {
       }
 
       if (komut == 'W') {
-        Serial.print(F("Cihaz Versiyon:"));
+        Serial.println(F("Konfigurasyon kaydediliyor..."));
+        delay(500);
         Serial.println(VERSIYON);
+       konfigurasyonYazdir();
         if (seridenAl()) {
               eepromaYaz();
             } else {
@@ -359,7 +322,7 @@ void KomutSatiriCalistir() {
 void parametreOku(char *szParam, int iMaxLen) {
   byte c;
   int iSize;
-  unsigned long iMilliTimeout = millis() + 1000; 
+  unsigned long iMilliTimeout = millis() + 2000; 
 
   for (iSize=0; iSize<iMaxLen; iSize++) szParam[iSize] = 0x00; 
   iSize = 0;   
@@ -368,7 +331,9 @@ void parametreOku(char *szParam, int iMaxLen) {
 
     if (Serial.available()) {
       c = Serial.read();
+
       if (c == 0x09 || c == 0x04) {
+        Serial.println();
         return;
       }
       if (iSize < iMaxLen) {
@@ -388,7 +353,6 @@ bool seridenAl() {
     if (Serial.read() == 0x01) {
       parametreOku(szParam, sizeof(szParam));
       if (strcmp(szParam, VERSIYON) != 0) {
-        Serial.print(F("PC:"));
         Serial.println(szParam);
           Serial.println(F("E99 Versiyonlar uyumsuz..."));
         return false;
@@ -416,7 +380,7 @@ bool seridenAl() {
       Ayarlar.APRS_Path2SSID = szParam[0];
 */
       //Symbol/Tab
-/*      parametreOku(szParam, 1);
+      parametreOku(szParam, 1);
       Ayarlar.APRS_Sembolu = szParam[0];
       parametreOku(szParam, 1);
       Ayarlar.APRS_SembolTabi = szParam[0];
@@ -424,7 +388,7 @@ bool seridenAl() {
       //BeaconTipi
       parametreOku(szParam, sizeof(szParam));
       Ayarlar.APRS_BeaconTipi = atoi(szParam);
-*/
+
       //Beacon Suresi
       parametreOku(szParam, sizeof(szParam));
       Ayarlar.APRS_BeaconSuresi = atoi(szParam);
@@ -434,8 +398,8 @@ bool seridenAl() {
       strcpy(Ayarlar.APRS_Mesaj, szParam);
 
 
-//      parametreOku(szParam, sizeof(szParam));
-//      Ayarlar.APRS_GPSSeriHizi = atoi(szParam);    
+      parametreOku(szParam, sizeof(szParam));
+      Ayarlar.APRS_GPSSeriHizi = atoi(szParam);    
 
 
       unsigned int iCheckSum = 0;
@@ -454,7 +418,6 @@ bool seridenAl() {
  * Elimizdeki CONFIG degiskenini byte byte PC'ye gonderiyoruz
  */
 void seriyeGonder() {
-
         Serial.print(F("{"));    //JSON Baslangici
         Serial.print(F("'V':'"));
         Serial.print(VERSIYON);
@@ -463,7 +426,7 @@ void seriyeGonder() {
         Serial.print(F("','CagriIsaretiSSID':"));
         Serial.print(Ayarlar.APRS_CagriIsaretiSSID);
         Serial.print(F(",'Destination':'"));
-/*        Serial.print(Ayarlar.APRS_Destination);
+        Serial.print(Ayarlar.APRS_Destination);
         Serial.print(F("','DestinationSSID':"));
         Serial.print(Ayarlar.APRS_DestinationSSID);
         Serial.print(F(",'Path1':'"));
@@ -474,14 +437,14 @@ void seriyeGonder() {
         Serial.print(Ayarlar.APRS_Path2);
         Serial.print(F("','Path2SSID':"));
         Serial.print(Ayarlar.APRS_Path2SSID);
-*/        //Symbol
+        //Symbol
         Serial.print(F(",'Sembol':'"));
         Serial.print(Ayarlar.APRS_Sembolu);
-//        Serial.print(F("','SembolTabi':'"));
-//        Serial.print(Ayarlar.APRS_SembolTabi);
+        Serial.print(F("','SembolTabi':'"));
+        Serial.print(Ayarlar.APRS_SembolTabi);
         //Beacon Type
-//        Serial.print(F("','BeaconTipi':"));
-//        Serial.print(Ayarlar.APRS_BeaconTipi, DEC);
+        Serial.print(F("','BeaconTipi':"));
+        Serial.print(Ayarlar.APRS_BeaconTipi, DEC);
         Serial.print(F(",'BeaconSuresi':"));
         //Beacon - Simple Delay
         Serial.print(Ayarlar.APRS_BeaconSuresi, DEC);
@@ -489,9 +452,8 @@ void seriyeGonder() {
         Serial.print(F(",'Mesaj':'"));
         Serial.print(Ayarlar.APRS_Mesaj);
         //GPS Serial Data
-        Serial.print(F("'"));
-//        Serial.print(F("','GPSHizi':"));
-//        Serial.print(Ayarlar.APRS_GPSSeriHizi, DEC);      
+        Serial.print(F("','GPSHizi':"));
+        Serial.print(Ayarlar.APRS_GPSSeriHizi, DEC);      
         Serial.print(F("}")); //JSON Sonu 
-
 }
+
